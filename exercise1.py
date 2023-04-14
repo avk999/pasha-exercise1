7a47f6434e6ff79ad9f593a0bef52ae3d0c1216b
Andrey Kovalev <andrey.kovalev@imc.com>
2023-04-14 08:32:43 +0000

    Moved command handling to the if block properly

--- exercise1.py
+++ exercise1.py
@@ -81,56 +81,56 @@ if __name__ == "__main__":
     5) exit
     """
 
-w = """Welcome to Pet manager! Please, choose command:
-1 - add animal;
-2 - search for an animal;
-3 - list all animals;
-4 - delete an animal;
-5 - exit
-Enjoy!"""
+    w = """Welcome to Pet manager! Please, choose command:
+    1 - add animal;
+    2 - search for an animal;
+    3 - list all animals;
+    4 - delete an animal;
+    5 - exit
+    Enjoy!"""
 
-print(w)
+    print(w)
 
-while True:
-    c = input("Enter command: ")
-    try:
-        command = int(c)
-    except ValueError:
-        print("Invalid command")
-        command = 0
-    if command == Commands.add:
-        id = int(input("ID: "))
-        name = input("NAME: ")
-        g = input("Gender (M/F): ")
-        gender_male = g == "M"
-        species = input("Species: ")
-        animal = Animal(id, gender_male, name, species)
-        result = mgr.add_animal(animal)
-        if result.success:
-            print("OK")
-        else:
-            print(f"Error: {result.message}")
+    while True:
+        c = input("Enter command: ")
+        try:
+            command = int(c)
+        except ValueError:
+            print("Invalid command")
+            command = 0
+        if command == Commands.add:
+            id = int(input("ID: "))
+            name = input("NAME: ")
+            g = input("Gender (M/F): ")
+            gender_male = g == "M"
+            species = input("Species: ")
+            animal = Animal(id, gender_male, name, species)
+            result = mgr.add_animal(animal)
+            if result.success:
+                print("OK")
+            else:
+                print(f"Error: {result.message}")
 
-    if command == Commands.list:
-        # al=mgr.list_animals(species != None) - Trying to insert values to method_list, but it doesn't work now.
-        g=input("Gender (M/F, empty for all)")# .strip()
-        # g M, F, ""
-        #True = M, False = F, None = ""
-        #gm= ....... 
-        print(f"DEBUG: gm={gm}")
-        sp=input("Species (empty for all): ")
-        sp=sp if sp != "" else None
-        print(f"DEBUG: sp={sp}")
-        
-        
-        al=mgr.list_animals(gender=gm, species=sp)
-        print(al)
-    if command == Commands.exit:
-        print("Goodbye!")
-        quit()
-    # Other commands here
-    if command == Commands.delete:
-        print ("Input ID:")
-        id_to_delete=int(input())
-        de=mgr.delete_animal(id_to_delete)
-        print(de)
+        if command == Commands.list:
+            # al=mgr.list_animals(species != None) - Trying to insert values to method_list, but it doesn't work now.
+            g=input("Gender (M/F, empty for all)")# .strip()
+            # g M, F, ""
+            #True = M, False = F, None = ""
+            #gm= ....... 
+            print(f"DEBUG: gm={gm}")
+            sp=input("Species (empty for all): ")
+            sp=sp if sp != "" else None
+            print(f"DEBUG: sp={sp}")
+            
+            
+            al=mgr.list_animals(gender=gm, species=sp)
+            print(al)
+        if command == Commands.exit:
+            print("Goodbye!")
+            quit()
+        # Other commands here
+        if command == Commands.delete:
+            print ("Input ID:")
+            id_to_delete=int(input())
+            de=mgr.delete_animal(id_to_delete)
+            print(de)
