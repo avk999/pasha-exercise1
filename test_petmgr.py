fa09b207a5f5231704391af12fe1498aea1b4950
Andrey Kovalev <andrey.kovalev@imc.com>
2023-04-14 08:36:04 +0000

    simple list tests passed

--- test_petmgr.py
+++ test_petmgr.py
@@ -1,13 +1,13 @@
 import pytest
 
 from exercise1 import PetManager
-from models.datamodel import Animal
+from modules.datamodel import Animal
 
 def test_init():
     t=PetManager()
     assert type(t) == PetManager
 
-
+@pytest.fixture()
 def populated_petmanager():
     t=PetManager()
     for a in [ 
@@ -16,12 +16,11 @@ def populated_petmanager():
                     (3,True, "Name3", "cat"),
                     (4,False, "Name4", "cat"),
                     ]:
-        t.add_animal(Animal(**a))
+        t.add_animal(Animal(*a))
     return t
 
-@pytest.fixture(populated_petmanager)
-def test_list_no_filters(mgr):
-    l=mgr.list_animals()
+def test_list_no_filters(populated_petmanager):
+    l=populated_petmanager.list_animals()
     assert len(l)==4
     ids=[x.id for x in l]
     assert set(ids) == set([1,2,3,4])
